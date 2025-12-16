9628deefb5d90173f82ebf8780d1b298317ecb48
Isaac Song <isaacsong03@gmail.com>
2025-12-16 01:01:23 -0500

    implement custom pattern solver for boardgame-qa

--- src/pvx/tasks/boardgame.py
+++ src/pvx/tasks/boardgame.py
@@ -2,7 +2,7 @@ import os
 
 from inspect_ai import Task, task
 from inspect_ai.dataset import Sample, hf_dataset
-from inspect_ai.scorer import model_graded_fact
+from inspect_ai.scorer import pattern
 from inspect_ai.solver import chain_of_thought, generate, system_message
 
 
@@ -34,14 +34,17 @@ def boardgame_loader():
             system_message(
                 "You are a helpful logic assistant. "
                 "Solve the logic puzzle step-by-step. "
-                "You must provide a final answer and the answer must be only one of the following words: 'proved', 'disproved', or 'unknown'."
+                "The last line poses the statement in question and you have to idenitfy if the statement is provably true, provably false, or neither. "
+                "You must provide a final answer and the answer must be one of the following words: 'proved', 'disproved', or 'unknown', for provably true, provable false, or neither provably true or false respectively."
             ),
             chain_of_thought(),
             generate(),
         ],
-        scorer=model_graded_fact(),
+        # scorer=model_graded_fact(),
+        scorer=pattern(
+            pattern="\\b(proved|disproved|unknown)\\b",
+        )
     )
-
 
 # 3. VERIFICATION BLOCK (Run this file directly to test)
 if __name__ == "__main__":
